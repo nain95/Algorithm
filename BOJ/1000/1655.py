145500fc80003224ff31d48b7e150ac2f549d34c
in_yeop <najio95@gmail.com>
2021-10-06 16:47:38 +0900

    solved 21.10.06

--- BOJ/1000/1655.py
+++ BOJ/1000/1655.py
@@ -1,18 +1,16 @@
-import sys,heapq
+import sys, heapq
 max_heap = []
 min_heap = []
 N = int(sys.stdin.readline())
 for i in range(N):
     data = int(sys.stdin.readline())
     if len(max_heap) == len(min_heap):
-        heapq.heappush(max_heap,(-data,data))
+        heapq.heappush(max_heap, -data)
     else:
         heapq.heappush(min_heap,data)
-    if len(min_heap) != 0 and max_heap[0][1] > min_heap[0]:
-        tmp1 = heapq.heappop(max_heap)[1]
-        tmp2 = heapq.heappop(min_heap)
-        heapq.heappush(min_heap,tmp1)
-        heapq.heappush(max_heap, (-tmp2,tmp2))
-    print(max_heap[0][1])
-
-
+    if len(min_heap) != 0 and -max_heap[0] > min_heap[0]:
+        max_value = -heapq.heappop(min_heap)
+        min_value = heapq.heappop(max_heap)
+        heapq.heappush(max_heap, max_value)
+        heapq.heappush(min_heap, min_value)
+    print(-max_heap[0])
