e151ed34fd3ea60b6a7852517bfdf0f58e3446f1
in_yeop <najio95@gmail.com>
2021-10-08 02:19:33 +0900

    solved 21.10.08

--- BOJ/1000/1967.py
+++ BOJ/1000/1967.py
@@ -2,31 +2,25 @@ import sys
 from collections import defaultdict, deque
 sys.setrecursionlimit(10**6)
 
-def dfs(start, cur, distance):
+def dfs(start, cur, distance, tmp):
     global leaf
     ck = 0
-    for node in dic[cur]:
-    
-        dist_dic[min(node,start)][max(node,start)] = dist_dic[min(cur,node)][max(cur,node)] + distance
-        dfs(start, node, distance + dist_dic[min(cur,node)][max(cur,node)])
-        ck = 1 
-    if ck == 0 and leaf[1] < dist_dic[min(start,cur)][max(start,cur)]:
-        leaf = [cur, dist_dic[min(start,cur)][max(start,cur)]]
+    for node in dic[cur].keys():
+        if node != tmp:
+            dfs(start, node, distance + dic[min(cur,node)][max(cur,node)], cur)
+            ck = 1 
+    if ck == 0 and leaf[1] < distance:
+        leaf = [cur, distance]
 
 v = int(sys.stdin.readline())
-dic = defaultdict(list)
-dist_dic = defaultdict(dict)
-for i in range(1,v+1):
-    dist_dic[i][i] = 0
+dic = defaultdict(dict)
 for _ in range(v-1):
-    parents, children, dist = map(int, sys.stdin.readline().split())
-    dic[parents].append(children)
-    dic[children].append(parents)
-    dist_dic[min(parents,children)][max(parents,children)] = dist
-leaf = [0,0]
-dfs(1, 1, 0)
-print(dic[1])
-print(dist_dic[1])
+    parents, children, dist = sys.stdin.readline().split()
+    dist = int(dist)
+    dic[parents][children] = dist
+    dic[children][parents] = dist
+leaf = ['0',0]
+dfs('1', '1', 0, '1')
 leaf[1] = -1
-dfs(leaf[0], leaf[0], 0)
-print(sys.getsizeof(dic))
+dfs(leaf[0], leaf[0], 0, leaf[0])
+print(leaf[1])
