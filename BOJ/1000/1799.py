f5f9d60d96e935e04b08d00d1ecc574cd6d4ce8f
in_yeop <najio95@gmail.com>
2021-12-05 21:50:04 +0900

    algorighm

---
+++ BOJ/1000/1799.py
@@ -0,0 +1,92 @@
+import sys
+import math
+
+def check(x: int, y : int, cur_matrix: list) -> int:
+    ratio = 1
+    while 0 <= x + (-1 * ratio) and 0 <= y + (-1  * ratio):
+        if cur_matrix[x + (-1 * ratio)][y + (-1 * ratio)] == 2:
+            return -1
+        ratio += 1
+    ratio = 1
+    while 0 <= x + (-1 * ratio) and y + (1  * ratio) < n:
+        if cur_matrix[x + (-1 * ratio)][y + (1 * ratio)] == 2:
+            return -1
+        ratio += 1
+    return 1
+
+def count_rest(x : int, y : int, matrix: list, type : int) -> int:
+    cnt = 0
+    if type == 0:   # white
+        for j in range(y, n ,2):
+            if matrix[x][j] == 1:
+                cnt += 1
+        for i in range(x+1, n):
+            tmp = 1
+            if i % 2 == 0:
+                tmp = 0
+            for j in range(tmp, n, 2):
+                if matrix[i][j] == 1:
+                    cnt += 1
+    else:
+        for j in range(y, n ,2):
+            if matrix[x][j] == 1:
+                cnt += 1
+        for i in range(x+1, n):
+            tmp = 0
+            if i % 2 == 0:
+                tmp = 1
+            for j in range(tmp, n, 2):
+                if matrix[i][j] == 1:
+                    cnt += 1
+    return cnt
+
+def dfs(x : int, y : int, matrix : list, ans : int, depth : int, cnt : int) -> int:
+    if ans > depth + cnt:
+        return ans
+    for i in range(x, n):
+        tmp = 1
+        if i % 2 == 0:
+            tmp = 0
+        for j in range(tmp, n, 2):
+            if matrix[i][j] == 1 and check(i, j, matrix) == 1:
+                tmp_matrix = matrix[:]
+                tmp_matrix[i][j] = 2
+                ans = max(depth, dfs(i, j, tmp_matrix, ans, depth + 1, cnt - 1))
+                ans = max(depth, dfs(i, j, matrix[:], ans, depth, cnt))
+                tmp_matrix[i][j] = 1
+                matrix = tmp_matrix[:]
+    return ans
+
+def dfs2(x : int, y : int, matrix : list, ans : int, depth : int, cnt : int) -> int:
+    if ans > depth + cnt:
+        return ans
+    for i in range(x, n):
+        tmp = 0
+        if i % 2 == 0:
+            tmp = 1
+        for j in range(tmp, n, 2):
+            if matrix[i][j] == 1 and check(i, j, matrix) == 1:
+                tmp_matrix = matrix[:]
+                tmp_matrix[i][j] = 2
+                ans = max(depth, dfs2(i, j, tmp_matrix, ans, depth + 1, cnt - 1))
+                ans = max(depth, dfs2(i, j, matrix[:], ans, depth, cnt))
+                tmp_matrix[i][j] = 1
+                matrix = tmp_matrix[:]
+    return ans
+
+if __name__ == '__main__':
+    n = int(sys.stdin.readline())
+    matrix = []
+    for i in range(n):
+        matrix.append(list(map(int, sys.stdin.readline().split())))
+    white = count_rest(0,0,matrix,0)
+    black = count_rest(0,0,matrix,1)
+    res = 0
+    res += dfs(0, 0, matrix, 0 , 1, white)
+    res += dfs2(0, 0, matrix, 0 , 1, black)
+    print(res)
+    #for i in range(n):
+    #    for j in range(n):
+    #        if matrix[i][j] == 1:
+    #            res = max(res, dfs(i, j, matrix, 0 , 2))
+    #print(res)
