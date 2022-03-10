5c17ab891ce488484eb455e5799d40c2ecbf3093
in_yeop <najio95@gmail.com>
2022-03-11 01:42:22 +0900

    solved 22.03.11

--- BOJ/1000/1005.py
+++ BOJ/1000/1005.py
@@ -1,26 +1,29 @@
+from collections import defaultdict, deque
 import sys
-from collections import deque
 
-string1 = list(sys.stdin.readline().rstrip())
-string2 = list(sys.stdin.readline().rstrip())
-matrix = [[0] * (len(string2) + 1) for _ in range(len(string1) + 1)]
-for i, str1 in enumerate(string1):
-    i += 1
-    for j, str2 in enumerate(string2):
-        j += 1
-        if str1 == str2:
-            matrix[i][j] = matrix[i-1][j-1] + 1
-        else:
-            matrix[i][j] = max(matrix[i-1][j], matrix[i][j-1])
-queue = deque()
-while matrix[i][j] != 0:
-    if matrix[i][j] == matrix[i-1][j]:
-        i -= 1
-    elif matrix[i][j] == matrix[i][j-1]:
-        j -= 1
-    else:
-        queue.appendleft(string1[i-1])
-        i -= 1
-        j -= 1
-print(len(queue))
-print("".join(queue))
+t = int(sys.stdin.readline())
+for _ in range(t):
+    n, k = map(int, sys.stdin.readline().split())
+    build_time = [0]+list(map(int, sys.stdin.readline().split()))
+    build_dic = defaultdict(list)
+    entry_level = [0] * (n + 1)
+    dp = [0] * (n + 1)
+    queue = deque()
+    for _ in range(k):
+        first, second = map(int, sys.stdin.readline().split())
+        build_dic[first].append(second)
+        entry_level[second] += 1
+    for i in range(1, n + 1): 
+        if entry_level[i] == 0:
+            queue.append(i)
+            dp[i] = build_time[i]
+    while queue:
+        now = queue.popleft()
+        for i in build_dic[now]: 
+            entry_level[i] -= 1 
+            dp[i] = max(dp[now] + build_time[i], dp[i])
+            if entry_level[i] == 0: 
+                queue.append(i)
+    ans = int(sys.stdin.readline())
+    print(dp[ans])
+
